5f298c0fb7a85d7a3111fc670d333cf44a57db52
Abhishek Dani <109074772+Abhishek-Dani@users.noreply.github.com>
2022-07-11 16:18:53 +0530

    Update streamlit_app.py
    fixed menu and typo mistakes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('my parents new healthy dinner at their place at 10pm')
+streamlit.title('my parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket smoothy')
+streamlit.text('Hard boiled free-range egg')
