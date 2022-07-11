14214b3cdc8867fc2af7f749738175654616a4ac
Abhishek Dani <109074772+Abhishek-Dani@users.noreply.github.com>
2022-07-11 16:27:39 +0530

    Update streamlit_app.py
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 and Blueberry oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket smoothy')
 streamlit.text('🐔Hard boiled free-range egg')
-
 streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
